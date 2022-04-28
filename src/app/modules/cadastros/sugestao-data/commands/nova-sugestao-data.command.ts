c6771710df9baa3cb423f753fe0a931af39f8e30
Altair Sossai <altair_sossai@outlook.com>
2022-04-28 14:26:42 +0000

    Classes para acessar os recursos de sugestão de datas

---
+++ src/app/modules/cadastros/sugestao-data/commands/nova-sugestao-data.command.ts
@@ -0,0 +1,3 @@
+export class NovaSugestaoDataCommand {
+    public data: Date | undefined;
+}
---
+++ src/app/modules/cadastros/sugestao-data/commands/responder-sugestao-data.command.ts
@@ -0,0 +1,5 @@
+import { RespostaTime } from "../../data-confronto/enums/resposta-time.enum";
+
+export class ResponderSugestaoDataCommand {
+    public resposta: RespostaTime | undefined;
+}
