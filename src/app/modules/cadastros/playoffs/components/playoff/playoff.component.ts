37816e73455339fa07f9ff744f64509357cf1efd
Altair Sossai <altair_sossai@outlook.com>
2022-07-23 10:08:23 -0300

    Funcionalidade de excluir

--- src/app/modules/cadastros/playoffs/components/playoff/playoff.component.ts
+++ src/app/modules/cadastros/playoffs/components/playoff/playoff.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { Playoff } from '../../models/playoff';
+import { PlayoffService } from '../../services/playoff.service';
 import { PlayoffEditComponent } from '../playoff-edit/playoff-edit.component';
 
 @Component({
@@ -14,10 +16,27 @@ export class PlayoffComponent {
   @Input() podeEditar = false;
   @Output() atualizado = new EventEmitter<any>();
 
-  constructor(private modalService: NzModalService) { }
+  constructor(
+    private modalService: NzModalService,
+    private messageService: NzMessageService,
+    private playoffService: PlayoffService) { }
 
   atualizar(): void {
     this.atualizado.emit();
+  }
+
+  excluir(playoffId: string): void {
+    this.modalService.confirm({
+      nzTitle: 'Deseja realmente excluir?',
+      nzOnOk: () => {
+        this.playoffService.delete(playoffId).subscribe(_ => {
+          this.atualizar();
+          this.messageService.create('success', 'Excluido com sucesso');
+        }, _ => {
+          this.messageService.create('error', 'Ocorreu um erro');
+        });
+      }
+    });
   }
 
   editar(playoffId: string): void {
