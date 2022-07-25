0d42ec2112e227044f1100071d5c98fabf55385d
Altair Sossai <altair_sossai@outlook.com>
2022-07-25 18:50:21 -0300

    Ajustes de layout para inicio do desenvolvimento

--- src/app/modules/cadastros/playoffs/components/playoff-match/playoff-match.component.ts
+++ src/app/modules/cadastros/playoffs/components/playoff-match/playoff-match.component.ts
@@ -1,4 +1,6 @@
+import { Time } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { Playoff } from '../../models/playoff';
 
 @Component({
   selector: 'app-playoff-match',
@@ -9,8 +11,15 @@ export class PlayoffMatchComponent implements OnInit {
 
   @Input() match: any;
 
+  time?: Time;
+  playoff?: Playoff;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.match.codigo)
+      this.time = this.match;
+    else
+      this.playoff = this.match;
   }
 }
