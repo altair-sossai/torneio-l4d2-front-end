582a3922f2673f531e5d7a93a23d4f095e3cbe0d
Altair Sossai <altairsossai@gmail.com>
2026-07-28 14:22:44 -0300

    fix: correct apiUrl to include HTTPS in production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "torneio-l4d2-function-h5dcdgffcjemejgr.brazilsouth-01.azurewebsites.net",
+  apiUrl: "https://torneio-l4d2-function-h5dcdgffcjemejgr.brazilsouth-01.azurewebsites.net",
   playstatsUrl: 'https://l4d2-playstats-api.azurewebsites.net'
 };
